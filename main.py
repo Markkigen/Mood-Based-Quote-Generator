fc698ad231bb42dc3eb855e8689d3af418050f5d
mark <markkipngeno01@gmail.com>
2025-05-11 18:38:09 +0300

    creted main .py

---
+++ main.py
@@ -0,0 +1,8 @@
+from quotes import get_quote
+
+print("🧠 Mood-Based Quote Generator 🧘‍♂️")
+mood = input("How are you feeling? (happy, sad, stressed, motivated): ")
+
+quote = get_quote(mood)
+print("\n✨ Here's a quote for you:")
+print(quote)
