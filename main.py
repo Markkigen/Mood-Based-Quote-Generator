ec42ffa35c72bb86407bfb0de538f8bb71b6c198
MARK KIPNGENO <140051551+Markkigen@users.noreply.github.com>
2025-05-29 20:27:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ print("Mood-Based Quote Generator ")
 mood = input("How are you feeling? (happy, sad, stressed, motivated): ")
 
 quote = get_quote(mood)
-print("\n✨ Here's a quote for you:")
+print("\n Here's a quote for you:")
 print(quote)
